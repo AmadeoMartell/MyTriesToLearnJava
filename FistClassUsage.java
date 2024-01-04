0985ee7418b404a7bd245e9dbae09c0550b95bd6
AmadeoMartell <medeu207@gmail.com>
2024-01-04 22:03:31 +0600

    Added getters/setter for use

--- FistClassUsage.java
+++ FistClassUsage.java
@@ -8,29 +8,62 @@ public class FistClassUsage {
         for (int i = 0; i < members; i++) {
             Group[i] = new FirstClass();
             System.out.println("Введите данные ученика в формате Barcode, Name, Age, GPA: ");
-            Group[i].id = console.nextInt();
-            Group[i].Name = console.next();
-            Group[i].age = console.nextInt();
-            Group[i].GPA = console.nextFloat();
+            Group[i].setId(console.nextInt());
+            Group[i].setName(console.next());
+            Group[i].setAge(console.nextInt());
+            Group[i].setGPA(console.nextFloat());
             System.out.println();
         }
         int MaxGpaID = 0;
-        float GPA = 0, MaxGPA = Group[0].GPA;
+        float GPA = 0, MaxGPA = Group[0].getGPA();
         for (int i = 0; i < members; i++) {
-            if (Group[i].GPA > MaxGPA) {
-                MaxGPA = Group[i].GPA;
+            if (Group[i].getGPA() > MaxGPA) {
+                MaxGPA = Group[i].getGPA();
                 MaxGpaID = i;
             }
-            GPA += Group[i].GPA;
+            GPA += Group[i].getGPA();
         }
         System.out.println("Average GPA of Group: " + (GPA/members));
-        System.out.println("The best Student: " + Group[MaxGpaID].Name + ". He/She got " +
-                Group[MaxGpaID].GPA + " Academic GPA");
+        System.out.println("The best Student: " + Group[MaxGpaID].getName() + ". He/She got " +
+                Group[MaxGpaID].getGPA() + " Academic GPA");
         // Some Garbage is Here for test
     }
 }
 class FirstClass {
-    public int id, age;
-    public String Name;
-    public float GPA;
+    private int id;
+    private int age;
+    private String Name;
+    private float GPA;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public float getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(float GPA) {
+        this.GPA = GPA;
+    }
 }
