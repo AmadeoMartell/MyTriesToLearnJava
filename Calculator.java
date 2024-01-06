74eb8dde6f756b0e250fd6dedf94014b000678f0
AmadeoMartell <medeu207@gmail.com>
2024-01-07 04:44:06 +0600

    added DecimalFormat, refactor of switch-case

--- Calculator.java
+++ Calculator.java
@@ -1,31 +1,33 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        DecimalFormat dF = new DecimalFormat("#.######");
         Scanner scan = new Scanner(System.in);
-        System.out.print("Введите уравнение: ");
-        float a = scan.nextFloat();
-        String sym = scan.next();
-        float b = scan.nextFloat();
-        switch (sym){
-            case "+":
-                System.out.print(a + b);
-                break;
-            case "-":
-                System.out.print(a - b);
-                break;
-            case "*":
-                System.out.print(a * b);
-                break;
-            case "/":
-                System.out.print(a / b);
-                break;
-            case "%":
-                System.out.print(a % b);
-                break;
-            default:
-                System.out.print("Вы ввели не правильный символ!");
-                break;
+        boolean statement = true;
+        ArrayList<String> calclog = new ArrayList<>();
+        while (statement){
+            System.out.print("Введите уравнение (Example 2 + 2): ");
+            float a = scan.nextFloat();
+            String sym = scan.next();
+            float b = scan.nextFloat();
+            switch (sym){
+                case "+" -> calclog.add("Ответ: " + dF.format(a + b));
+                case "-" -> calclog.add("Ответ: " + dF.format(a - b));
+                case "*" -> calclog.add("Ответ: " + dF.format(a * b));
+                case "/" -> calclog.add("Ответ: " + dF.format(a / b));
+                case "%" -> calclog.add("Ответ: " + dF.format(a % b));
+                default -> {
+                    calclog.add("Вы ввели не правильный символ!");
+                    statement = false;
+                }
+            }
         }
+        OutputWriter.write(calclog);
+//        for (String iter: calclog){
+//            System.out.println(iter);
+//        }
     }
 }
